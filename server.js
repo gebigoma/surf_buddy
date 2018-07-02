4765d466e8c36b810664f6b5fb08362253a6958f
Andrew Bao <bao.andrew@gmail.com>
2018-07-01 20:55:55 -0700

    Set up passport in server.js and added isLoggedIn function in user ctrl

--- server.js
+++ server.js
@@ -10,11 +10,31 @@ const
   cookieParser = require('cookie-parser'),
   session = require('express-session'),
   MongoDBStore = require('connect-mongodb-session')(session), 
+  passport = require('passport'), 
+  passportConfig = require('./config/passport.js'),
   PORT = 3000
 
+//middleware 
+app.use(cookieParser())
+app.use(logger('dev'))
 // environment port
 
 // mongoose connection
+const store = new MongoDBStore({
+  uri: mongoConnectionString,
+  collection: 'sessions'
+})
+//sessions 
+app.use(session({
+  secret: "Shhh",
+  cookie: {maxAge: 60000000 },
+  resave: true,
+  saveUninitialized: false,
+  store: store
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // ejs configuration
 app.set('view engine', 'ejs')
