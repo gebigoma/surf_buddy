71ac73d199096abf158bf21b21a8b5df106b8900
Tina <tparkmedia@gmail.com>
2018-07-02 11:08:34 -0700

    added comments for guideline gitflow

--- controllers/UsersCtrl.js
+++ controllers/UsersCtrl.js
@@ -1,12 +1,27 @@
 const 
     User = require('../models/User')
 
-exports.new = (req, res) => {
+
+// show controller
+
+    exports.new = (req, res) => {
     User.find({}, (err, user) => {
         if(err) res.json({status: "FAIL", payload: err})
         res.json({status: 'SUCCESS', payload: user})
     })
 }
+
+
+// create controller
+
+
+// patch controller
+
+
+// delete controller
+
+
+
 
 
 
--- routers/Users.js
+++ routers/Users.js
@@ -4,6 +4,13 @@ const
     usersRouter = express.Router(),
     users = require('../controllers/UsersCtrl')
 
+// index router
+// show router
 usersRouter.get('/signup', users.new) 
+// create router
+// edit router
+// update router
+// delete router
+
 
 module.exports = usersRouter;
