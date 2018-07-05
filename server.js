a671673e919c4db4feb409c6ab3e481afa5785b4
Alexander Rowland <arowland900@gmail.com>
2018-07-05 15:33:25 -0700

    added /css

--- server.js
+++ server.js
@@ -67,6 +67,7 @@ app.use(cookieParser())
 app.use(logger('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 //sessions 
 app.use(session({
